04c92887e6bb68ce5e20b4f67aa91b81c99d71bb
lee4339 <skyho4339@naver.com>
2022-06-07 21:33:17 +0900

    22.06.07수업내용

--- 0412/1/일반적인슬라이드/js/slidebanner_op.js
+++ 0412/1/일반적인슬라이드/js/slidebanner_op.js
@@ -46,47 +46,12 @@ $(document).ready(function () { //외부시트 적용시에는 무조건 적용
         var currentEl = $('#imgs > ul > li').eq(current); 
         var nextEl = $('#imgs > ul > li').eq(i);
 
-        currentEl.stop().css({ left: '0%' }).animate({ left: '-100%' });
-        nextEl.stop().css({ left: '100%' }).animate({ left: '0%' });
+        currentEl.stop().css({ opacity: 1 }).animate({ opacity: 0 });
+        nextEl.stop().css({ opacity: 0 }).animate({ opacity: 1 });
 
         current = i;
 
     }
-
-    function move2(i) {
-        if(current == i) return; 
-        // 현재 보이는 슬라이드와 클릭한 버튼의 인덱스값이 같다면
-        // 이 다음 코딩은 수행하지 않고 나감
-        
-        var currentEl = $('#imgs > ul > li').eq(current); 
-        var nextEl = $('#imgs > ul > li').eq(i);
-
-        currentEl.stop().css({ left: '0%' }).animate({ left: '100%' });
-        nextEl.stop().css({ left: '-100%' }).animate({ left: '0%' });
-
-        current = i;
-
-    }
-
-    $('.next').click(function () {
-        i = i + 1;
-        if (i == 3) { //최대값이 되었을때 최소값으로 이동을 제어
-            i = 0
-        }
-        move(i);
-        $('.btns > li').removeClass('on');
-        $('.btns > li').eq(i).addClass('on');
-    })
-
-    $('.prev').click(function () {
-        i = i + 1;
-        if (i == 3) { //최소값이 되었을때 최소값으로 이동을 제어 
-            i = 0;
-        }
-        move2(i);
-        $('.btns > li').removeClass('on');
-        $('.btns > li').eq(i).addClass('on');
-    })
 
     
 }); 
